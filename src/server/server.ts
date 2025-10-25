8c6ca1a0ae7fd9a766e7a13c64066691d7df2258
Thiago <92751944+Teles23@users.noreply.github.com>
2025-10-25 20:38:50 -0300

    chore(server): importar rotas de produtos e inicializar servidor

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,12 @@
 import cors from "cors";
 import express from "express";
+import { productRouter } from "../routes/product/product.routes.js";
 
 const server = express();
 
 server.use(cors());
 server.use(express.json());
+
+server.use("/api/products", productRouter);
 
 export default server;
