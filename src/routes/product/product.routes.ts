199f68638eab66ddaf0f21ea430eb7bc8b90d557
Thiago <92751944+Teles23@users.noreply.github.com>
2025-10-25 20:38:16 -0300

    feat(routes): configurar rotas de produtos com middleware de validação

---
+++ src/routes/product/product.routes.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { ProductController } from "../../controllers/product/product.controller.js";
+import { validate } from "../../middlewares/validateRequest.js";
+import {
+  createProductSchema,
+  idParamSchema,
+  updateProductSchema,
+} from "../../schemas/product.schema.js";
+const productRouter = express();
+
+productRouter.post(
+  "/",
+  validate(createProductSchema, "body"),
+  ProductController.createProduct
+);
+productRouter.get("/", ProductController.listProducts);
+productRouter.get(
+  "/:id",
+  validate(idParamSchema, "params"),
+  ProductController.getProductById
+);
+productRouter.put(
+  "/:id",
+  validate(idParamSchema, "params"),
+  validate(updateProductSchema, "body"),
+  ProductController.updateProduct
+);
+productRouter.delete(
+  "/:id",
+  validate(updateProductSchema, "params"),
+  ProductController.deleteProduct
+);
+
+export { productRouter };
