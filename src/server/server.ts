041ed95a8cbd7d67e766d0438eff0cff483ae09b
Thiago <92751944+Teles23@users.noreply.github.com>
2025-10-27 19:34:15 -0300

    feat(server): configura servidor principal e integração de rotas
    - Configura servidor Express e middlewares principais
    - Integra rotas de Menu, Produto e Promoção
    - Adiciona suporte a JSON e tratamento de erros globais
    - Prepara estrutura para logs e variáveis de ambiente

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
+import { menuRouter } from "../routes/menu/menu.routes.js";
 import { productRouter } from "../routes/product/product.routes.js";
+import { promotionRouter } from "../routes/promotion/promotion.routes.js";
 
 const server = express();
 
@@ -8,5 +10,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/api/products", productRouter);
+server.use("/api/promotions", promotionRouter);
+server.use("/api/menu", menuRouter);
 
 export default server;
