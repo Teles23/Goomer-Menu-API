ad139528b74c73162539e4cb88faad5635d87937
Thiago <92751944+Teles23@users.noreply.github.com>
2025-10-28 22:16:13 -0300

    refactor(server): ajustar inicialização do servidor para uso com Docker e log padronizado

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import server from "./server/server.js";
 const port = Number(process.env.PORT);
 
 server.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${port}`);
 });
